6c7af29332185122202de37e6f2269c9639056b7
Alexey Veselovsky <alexveselovsky95@gmail.com>
2023-02-27 21:39:11 +0200

    Update AddContacts.js

--- src/components/AddContacts/AddContacts.js
+++ src/components/AddContacts/AddContacts.js
@@ -20,13 +20,16 @@ class AddContacts extends Component {
   };
 
   render() {
+    //* value={name} в інпуті при скиданні state оновлює поле інпута
+    const { name, number } = this.state;
     return (
-      <form onSubmit={this.handleSubmit} autoComplete="off">
+      <form onSubmit={this.handleSubmit} autoComplete="on">
         <label>
           Name
           <input
             type="text"
             name="name"
+            value={name}
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
@@ -39,6 +42,7 @@ class AddContacts extends Component {
           <input
             type="tel"
             name="number"
+            value={number}
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
